ff6f4c84931bf82f725f9dfbfd1c608a3b4f77b7
김동혁 <briankim0910@gmail.com>
2024-02-13 21:23:21 +0900

    조건에 맞게 수열 변환하기 3.py

--- 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.py
+++ 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.py
@@ -1,8 +1,8 @@
 def solution(arr, k):
     answer = []
-    for i in range(len(arr)):
-        if k % 2 == 0:
-            arr[i] = arr[i] + k
+    for i in range(len(arr)):    #반복문으로 리스트 각각의값 계산
+        if k % 2 == 0:    
+            arr[i] = arr[i] + k     
             answer = arr
         else:
             arr[i] = arr[i] * k
