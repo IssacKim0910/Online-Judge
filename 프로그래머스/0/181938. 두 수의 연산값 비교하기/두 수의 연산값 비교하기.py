c8a3983729e7daa42511d1b9f5d5ef8ff389b4f6
김동혁 <briankim0910@gmail.com>
2024-02-13 21:39:01 +0900

    Update 두 수의 연산값 비교하기.py

--- 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.py
+++ 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.py
@@ -1,2 +1,12 @@
 def solution(a, b):
+    answer = 0
+    #문자형으로 바꾸면 숫자그대로 더해지기때문에 str로 변환
+    if int(str(a)+str(b)) > 2*a*b : 
+        answer = int(str(a)+str(b)) 
+    else:
+        answer = 2*a*b
+    return answer
+
+#다른 풀이
+def solution(a, b):
     return max(int(str(a) + str(b)), 2 * a * b)
